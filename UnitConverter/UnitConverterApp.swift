3c840ebdea3d7bb16ed43a118fb4b23344928448
Ikbal Demirdoven <ikbaldemirdoven@icloud.com>
2023-01-13 01:06:02 -0800

    Initial Commit

---
+++ UnitConverter/UnitConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConverterApp.swift
+//  UnitConverter
+//
+//  Created by Ikbal Demirdoven on 2023-01-13.
+//
+
+import SwiftUI
+
+@main
+struct UnitConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
